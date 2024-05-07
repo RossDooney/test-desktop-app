9c9ebfcf62d164b59b6ad485a494a576de91e793
Ross Dooney <rossdooney@gmail.com>
2024-05-07 17:30:17 +0100

    clean up

--- main.py
+++ main.py
@@ -4,6 +4,36 @@ from PIL import ImageTk
 bg_color = "#3d6466"
 
 
+def load_frame1():
+    frame1.pack_propagate(False)
+
+    logo_img = ImageTk.PhotoImage(file="assets/RRecipe_logo.png")
+    logo_widget = tk.Label(frame1, image=logo_img, bg=bg_color)
+    logo_widget.image = logo_img
+
+    logo_widget.pack()
+
+    tk.Label(
+        frame1,
+        text="Placeholder text here!!!",
+        bg=bg_color,
+        fg="white",
+        font=("TkMenuFont", 14),
+    ).pack()
+
+    tk.Button(
+        frame1,
+        text="More Placeholder",
+        font=("TkHeadingFont", 14),
+        bg="#28393a",
+        fg="white",
+        cursor="hand2",
+        activebackground="#badee2",
+        activeforeground="black",
+        command=lambda: load_frame2(),
+    ).pack(pady=20)
+
+
 def load_frame2():
     print("asd")
 
@@ -14,34 +44,12 @@ root.title("Test App")
 root.eval("tk::PlaceWindow . center")
 
 frame1 = tk.Frame(root, width=500, height=600, bg=bg_color)
-frame1.grid(row=0, column=0)
-frame1.pack_propagate(False)
+frame2 = tk.Frame(root, bg=bg_color)
 
-logo_img = ImageTk.PhotoImage(file="assets/RRecipe_logo.png")
-logo_widget = tk.Label(frame1, image=logo_img, bg=bg_color)
-logo_widget.image = logo_img
+for frame in (frame1, frame2):
+    frame.grid(row=0, column=0)
 
-logo_widget.pack()
-
-tk.Label(
-    frame1,
-    text="Placeholder text here!!!",
-    bg=bg_color,
-    fg="white",
-    font=("TkMenuFont", 14),
-).pack()
-
-tk.Button(
-    frame1,
-    text="More Placeholder",
-    font=("TkHeadingFont", 14),
-    bg="#28393a",
-    fg="white",
-    cursor="hand2",
-    activebackground="#badee2",
-    activeforeground="black",
-    command=lambda: load_frame2(),
-).pack(pady=20)
+load_frame1()
 
 # run app
 root.mainloop()
